c2a35d0e7c26cc7264beb1380627d712f4a8d0a9
Basavaraj <basavaraj2k00@gamil.com>
2021-09-21 20:11:06 +0530

    initial commit

--- hrs/settings.py
+++ hrs/settings.py
@@ -104,22 +104,22 @@ WSGI_APPLICATION = 'hrs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hrsdb',
-        'USER': 'postgres',
-        'PASSWORD': 'yr2327yr',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hrsdb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'yr2327yr',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
